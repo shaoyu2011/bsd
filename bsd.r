48ea11fe32552a38392cd6d404afc7128d332b3e
ahmedfsalhin <af.salhin@gmail.com>
2014-07-21 19:23:08 +0100

    lags_are_ready

--- bsd.r
+++ bsd.r
@@ -1,12 +1,15 @@
-rm(list=ls())
-library("foreign", lib.loc="H:/R/R-3.1.0/library")
-library("gdata", lib.loc="H:/R/R-3.1.0/library")
-library("astsa", lib.loc="H:/R/R-3.1.0/library")
-library("graphics", lib.loc="H:/R/R-3.1.0/library")
-library("MASS", lib.loc="H:/R/R-3.1.0/library")
-library("stats", lib.loc="H:/R/R-3.1.0/library")
-library("TTR", lib.loc="H:/R/R-3.1.0/library")
-library("zoo", lib.loc="H:/R/R-3.1.0/library")
+rm(list=ls())   
+library(foreign)
+library(gdata)
+library(astsa)
+library(graphics)
+library(MASS)
+library(stats)
+library(zoo)
+library(TTR)
+library(pastecs)
+library(dplyr)
+library(stringr)
 bsd<-read.dta("H:\\Ahmed\\Thesis\\bsd\\bsd.dta")
 names<-bsd[1,]
 colnames(bsd)<-names
@@ -17,17 +20,23 @@ bsd[dots]<-NA
 rm(dots)
 bsd<-sapply(bsd, as.numeric)
 bsd<-data.frame(bsd)
-sample<-bsd[36:75,c(1,6:12)]
+sample<-bsd[35:74,c(1,6:12)]
 row.names(sample) <- NULL
-sample<-sample[,c(1,5,8,3,4,6,2)]
-colnames(sample)[3]<-"Turn"
-sample<-transform(sample, Turn= Turn*100, s= s*100)
+sample<-sample[,c(1,5,8,3,4,6,2)]  # rearrange the sample to match the paper order
+colnames(sample)[3]<-"turn"
+sample<-transform(sample, turn= turn*100, s= s*100)
 #sample$Turn[40]<-NA
 #sample$ripo[40]<-NA
 #sample$pdnd[40]<-NA
-s.desc<-stat.desc(sample[-1])               # drop year
-s.desc<-s.desc[c(-1:-3,-6:-8,-10:-12,-14),] # get only mean,min,max and sd
-s.desc<-t(s.desc)                           # traspose the summary stats
-s.desc<-data.frame(s.desc)
-s.desc<-s.desc[c(3,4,1,2)]
-cormat<-as.matrix(cor(sample[,-1], use="complete.obs"))
+sample.desc<-stat.desc(sample[-1])                     # drop year
+sample.desc<-sample.desc[c(-1:-3,-6:-8,-10:-12,-14),]  # get only mean,min,max and sd
+sample.desc<-t(sample.desc)                            # traspose the summary stats
+sample.desc<-data.frame(sample.desc)                   # change to data frame
+sample.desc<-sample.desc[c(3,4,1,2)]                   # rearange  to match paper
+s.ts<-sample %>% mutate_each(funs(lead(.,1))) %>% cbind(sample, .) #get the leading values beside the original
+s.ts<-s.ts[,-8] #remove the repeted years
+colnames(s.ts)[8:13]<-str_c("l", colnames(s.ts)[2:7])  # change the column name
+s.ts[1,2;7]<-NA   # chage the first value of the original values
+s.ts<-s.ts[c(1,2,8,3,9,4,10,5,11,6,12,7,13)]  # rearrange the columns so each variable beside its lead
+
+
